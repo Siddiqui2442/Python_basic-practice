26ab8bee613141ee35b0d08099af2be92b8a08cd
Siddiqui2442 <77726402+Siddiqui2442@users.noreply.github.com>
2021-01-20 19:33:22 +0900

    Add files via upload

---
+++ Kansu.py
@@ -0,0 +1,40 @@
+#今回から関数を学習
+
+
+#No.15-start
+#関数=処理.色々な「処理」をまとめて1つにしたもの
+#同じ物を2回書く必要ない.1行で使い回しができる.他の人も使うことができる
+#2種類の関数がある:自分で作る関数とPythonが用意してくれている関数(組み込み関数)
+#関数を作る=関数を定義する
+#def 関数名(引数):.def 関数名(引数1,引数2):
+     #実行する処理(return文)
+
+#def say_hello(greeting):
+    #print(greeting)
+#say_hello("Hello World")
+
+#関数を変数に代入することができる
+#定義した関数名をそのまま変数に代入するだけ
+
+#def say_hello(greeting):
+    #print(greeting)
+
+#hello = say_hello
+#hello("Good Moring")
+
+#引数を2つ使用した場合
+#def add(num01,num02):
+    #print(num01+num02)
+    #return(num01+num02)
+
+#add(6,2)
+#print(add(6,2))
+#add_result=add(6,2)
+#print(add_result)
+
+#def div(a,b,c):
+    #return(a+b+c)/3
+
+#div_result = div(9,4,2)
+#print(div_result)
+#No.15-End
